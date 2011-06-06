4828ea99966e227c487a1310da178b0a2a319917
Spencer Steffen <spencer@citrusme.com>
2011-06-06 16:08:42 -0700

    more tests

--- test/integration/pages_integration_test.rb
+++ test/integration/pages_integration_test.rb
@@ -2,25 +2,27 @@ require_relative '../test_helper'
 
 class PagesIntegrationTest < ActiveSupport::IntegrationCase
   
+  setup do
+    Page.destroy_all
+  end
+  
   context "the homepage" do
   
     setup do
-      Page.destroy_all
       @home = Page.create(:title => "Home", :meta_title => "Welcome to our homepage!", :path => "/")
       @home.contents.first.update_attributes(:body => "This is a test", :context => "main")
       @home.contents.create(:title => "Some might say...", :body => "This is another test", :context => "intro")
       Dir[File.expand_path("../../../lib/tasks/sample", __FILE__) + "/*.jpg"].each {
         |image| @home.images.create(:attachment => File.open(image), :alt => "Sailing")
       }
+      visit root_path
     end
     
     should "have proper page title" do
-      visit root_path
       assert_title "Spree Demo Site - Welcome to our homepage!"      
     end
     
     should "have proper contents" do
-      visit root_path
       within ".left .content-main" do
         assert_seen "Home", :within => "h1.title"
         assert_seen "This is a test", :within => "p"
@@ -32,11 +34,31 @@ class PagesIntegrationTest < ActiveSupport::IntegrationCase
     end
         
     should "have a images in slideshow" do
-      visit root_path
       within "#content .slideshow" do
         @home.images.each do |img|
           assert has_xpath?("//img[@src='#{img.attachment.url(:slide)}']")
         end
+      end
+    end
+    
+  end
+  
+  context "any other page" do
+    
+    setup do
+      @page = Page.create(:title => "Some Page", :path => "/some-page")
+      @page.contents.first.update_attributes(:body => "OMG it really is a page")
+      visit @page.path
+    end
+    
+    should "have proper page title" do
+      assert_title "Spree Demo Site - Some Page"      
+    end
+    
+    should "have proper content" do
+      within ".content-main" do
+        assert_seen "Some Page", :within => "h1.title"
+        assert_seen "OMG it really is a page", :within => "p"
       end
     end
     
