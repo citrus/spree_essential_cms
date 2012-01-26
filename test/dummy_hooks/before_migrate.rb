6c9ca19da41ac0cf259923ef4a689671f9029daf
Spencer Steffen <spencer@citrusme.com>
2012-01-25 18:47:30 -0800

    update dummier before_migrate hook to only copy spree`s migrations

--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -1,13 +1,16 @@
-run "bundle exec rails g spree:site"
-run "bundle exec rails g spree_essentials:install"
-run "bundle exec rails g spree_essentials:cms"
+rake "spree:install:migrations"
+
+insert_into_file File.join('config', 'routes.rb'), :after => "Application.routes.draw do\n" do
+  "  # Mount Spree's routes\n  mount Spree::Core::Engine, :at => '/'\n"
+end
+
+run "rails g spree_essentials:install"
+run "rails g spree_essentials:cms"
 
 # remove all stylesheets except core  
 %w(admin store).each do |ns|
   js  = "app/assets/javascripts/#{ns}/all.js"
   css = "app/assets/stylesheets/#{ns}/all.css"
-  remove_file js
-  remove_file css
-  template "#{ns}/all.js", js
-  template "#{ns}/all.css", css
+  template "#{ns}/all.js", js, :force => true
+  template "#{ns}/all.css", css, :force => true
 end
