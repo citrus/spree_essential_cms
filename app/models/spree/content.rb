fce47eff2c7a26e82186f9d0decdc51a64cb2ae3
Spencer Steffen <spencer@citrusme.com>
2012-03-06 11:01:13 -0800

    remove support for spree_heroku. we`ll let the dev`s decide hohow to handle heroku on their own

--- app/models/spree/content.rb
+++ app/models/spree/content.rb
@@ -6,27 +6,16 @@ class Spree::Content < ActiveRecord::Base
 
   default_scope order(:position)
 
-  if defined?(SpreeHeroku)
-    has_attached_file :attachment,
-      :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
-      :default_style => :preview,
-      :path => "assets/contents/:id/:style/:basename.:extension",
-      :storage => "s3",
-      :s3_credentials => "#{Rails.root}/config/s3.yml"
-  else
-    has_attached_file :attachment,
-      :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
-      :default_style => :preview,
-      :url => "/spree/contents/:id/:style/:basename.:extension",
-      :path => ":rails_root/public/spree/contents/:id/:style/:basename.:extension"
-  end
-
+  has_attached_file :attachment,
+    :styles        => Proc.new{ |clip| clip.instance.attachment_sizes },
+    :default_style => :preview,
+    :url           => "/spree/contents/:id/:style/:basename.:extension",
+    :path          => ":rails_root/public/spree/contents/:id/:style/:basename.:extension"
+  
   cattr_reader :per_page
   @@per_page = 10
 
-
   scope :for, Proc.new{|context| where(:context => context)}
-
 
   [ :link_text, :link, :body ].each do |property|
     define_method "has_#{property.to_s}?" do
@@ -46,11 +35,9 @@ class Spree::Content < ActiveRecord::Base
     self.hide_title == true
   end
 
-
   def rendered_body
     RDiscount.new(body.to_s).to_html.html_safe
   end
-
 
   def default_attachment_sizes
     { :mini => '48x48>', :medium => '427x287>' }
@@ -65,8 +52,6 @@ class Spree::Content < ActiveRecord::Base
     end
     sizes
   end
-
-
 
   def context=(value)
     write_attribute :context, value.to_s.parameterize
