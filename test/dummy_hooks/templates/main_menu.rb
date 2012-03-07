325fd3bb8142c367000b6792ea6b62a7db208698
Spencer Steffen <spencer@citrusme.com>
2012-03-06 20:28:55 -0800

    fix main menu and remove main menu styles

--- test/dummy_hooks/templates/main_menu.rb
+++ test/dummy_hooks/templates/main_menu.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
-                     :name          => "spree_essential_cms_main_menu",
-                     :insert_bottom => "#header[data-hook]",
-                     :partial       => "spree/shared/main_menu",
+Deface::Override.new(:virtual_path  => "spree/shared/_store_menu",
+                     :name          => "spree_essential_cms_main_menu_items",
+                     :insert_after  => "#home-link[data-hook]",
+                     :partial       => "spree/shared/main_menu_items",
                      :disabled      => false)
