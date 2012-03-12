d633e913629cf857b0e00fdd24af83e863cdd83e
Spencer Steffen <spencer@citrusme.com>
2012-03-12 12:51:32 -0700

    reorganize dummier templates

--- test/dummy_hooks/after_app_generator.rb
+++ test/dummy_hooks/after_app_generator.rb
@@ -1,1 +1,17 @@
-copy_file "main_menu.rb", "app/overrides/main_menu.rb"
+# Add our main menu override
+copy_file "overrides/main_menu.rb", "app/overrides/main_menu.rb"
+
+# Fix uninitialized constant Spree::User::DestroyWithOrdersError 
+template "initializers/spree_user_error_fix.rb", "config/initializers/spree_user_error_fix.rb"
+
+# remove all stylesheets except core  
+%w(admin store).each do |ns|
+  template "assets/javascripts/#{ns}/all.js",  "app/assets/javascripts/#{ns}/all.js",  :force => true
+  template "assets/stylesheets/#{ns}/all.css", "app/assets/stylesheets/#{ns}/all.css", :force => true
+end
+
+# Fix sass load error by using the converted css file
+template "assets/stylesheets/store/screen.css", "app/assets/stylesheets/store/screen.css"
+
+# Enable forgery_protection since we need AUTH_TOKEN to be defined to avoid JS errors
+gsub_file "config/environments/test.rb", "forgery_protection    = false", "forgery_protection = true"
--- test/dummy_hooks/before_migrate.rb
+++ test/dummy_hooks/before_migrate.rb
@@ -6,21 +6,6 @@ insert_into_file File.join('config', 'routes.rb'), :after => "Application.routes
   "  # Mount Spree's routes\n  mount Spree::Core::Engine, :at => '/'\n"
 end
 
-# Fix uninitialized constant Spree::User::DestroyWithOrdersError 
-template "spree_user_error_fix.rb", "config/initializers/spree_user_error_fix.rb"
-
-# remove all stylesheets except core  
-%w(admin store).each do |ns|
-  template "#{ns}/all.js",  "app/assets/javascripts/#{ns}/all.js",  :force => true
-  template "#{ns}/all.css", "app/assets/stylesheets/#{ns}/all.css", :force => true
-end
-
-# Fix sass load error by using the converted css file
-template "store/screen.css", "app/assets/stylesheets/store/screen.css"
-
-# Enable forgery_protection since we need AUTH_TOKEN to be defined to avoid JS errors
-gsub_file "config/environments/test.rb", "forgery_protection    = false", "forgery_protection = true"
-
 # Install spree essentials & CMS
 run "bundle exec rails g spree_essentials:install"
 run "bundle exec rails g spree_essentials:cms"
